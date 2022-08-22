d3cd11dbbf28124b45b4377ff82006807e367c72
Johnson <johnsonkarasanyi@gmail.com>
2022-08-22 16:43:58 +0200

    created featured component

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,13 @@
+import './featured.css'
+const Featured =()=>{
+    return (
+        <div className='featured'>
+            <div className="featuredItem">
+              <img src="https://cf.bstatic.com/xdata/images/hotel/square200/368703637.webp?k=a385edc5cafda8beb7ab64cc78921720e59b41de417ff73efb946d3b918b13b0&o=&s=1" alt="" />
+              <h1>Dublin</h1>
+              <h2>123 Properties</h2>
+            </div>
+        </div>
+    )
+}
+export default Featured
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,18 @@
 import React from "react"; // React from v17 there is no need to import React
+import Featured from "../../components/featured/Featured";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navBar/Navbar";
-import './home.css'
+import "./home.css";
 
-const Home =()=>{
-return(
-   <>
-   <Navbar />
-   <Header/>
-   </>
-)
-}
-export default Home
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <div className="homeContainer">
+        <Featured />
+      </div>
+    </div>
+  );
+};
+export default Home;
