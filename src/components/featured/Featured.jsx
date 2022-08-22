f982fd8ec59a9f88f51d5bce1e89a6785b3da857
Johnson <johnsonkarasanyi@gmail.com>
2022-08-22 18:08:23 +0200

    added property component

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -3,21 +3,21 @@ const Featured =()=>{
     return (
         <div className='featured'>
             <div className="featuredItem">
-              <img className='featuredImg' src="https://cf.bstatic.com/xdata/images/hotel/square200/368703637.webp?k=a385edc5cafda8beb7ab64cc78921720e59b41de417ff73efb946d3b918b13b0&o=&s=1" />
+              <img className='featuredImg' src="https://cf.bstatic.com/xdata/images/hotel/square200/291054565.webp?k=e40283077d9421399b599ecf35b7bf0d4698ad851c8d238db7759ba0feb224af&o=&s=1" alt='' />
               <div className="featuredTitles">
               <h1>Dublin</h1>
               <h2>123 Properties</h2>
               </div>
             </div>
             <div className="featuredItem">
-              <img className='featuredImg' src="https://cf.bstatic.com/xdata/images/hotel/square200/207751165.webp?k=4d52b9e658d9cba792be3d17493ca714c3d421f6e83bdc5b86f2d9c36095959b&o=&s=1"  />
+              <img className='featuredImg' src="https://cf.bstatic.com/xdata/images/hotel/square200/321192147.webp?k=ca7d75fa872672f75dc0591cbcdaf62480a72a11d72df402744aad309b2d21fe&o=&s=1" alt='' />
              <div className="featuredTitles">
              <h1>Austin</h1>
               <h2>523 Properties</h2>
              </div>
             </div>
             <div className="featuredItem">
-              <img className='featuredImg' src="https://cf.bstatic.com/xdata/images/hotel/square200/44754339.webp?k=3dd0784cd235cc75b6ed0bae017d7bdd8696e1a698077ee4c054bdcf0ccd4cdd&o=&s=1" />
+              <img className='featuredImg' src="https://cf.bstatic.com/xdata/images/hotel/square200/339442125.webp?k=ed2359e9411ceac47475235c14b2628e854c9ae6c61f757c793eebdd1e5fb4a4&o=&s=1" alt='' />
              <div className="featuredTitles">
              <h1>Reno</h1>
               <h2>533 Properties</h2>
---
+++ src/components/propertyList/PropertyList.jsx
@@ -0,0 +1,44 @@
+import './propertyList.css'
+
+const PropertyList =()=>{
+    return(
+        <div className="pList">
+            <div className="pListItem">
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square200/384050431.webp?k=c16a143c955af442e9a88cfe699627f2725dbf2e38bfbc809149384ca079161b&o=&s=1" alt="" className="pListImg" />
+                <div className="pListTitles">
+                    <h1>Hotels</h1>
+                    <h2>234 hotels</h2>
+                </div>
+                </div>
+                <div className="pListItem">
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square200/289702574.webp?k=9335f0dad45af7e8a71b6d453076cec89679bc9cc348c4fbd10afa75614704b4&o=&s=1" alt="" className="pListImg" />
+                <div className="pListTitles">
+                    <h1>Resolts</h1>
+                    <h2>234 hotels</h2>
+                </div>
+            </div>
+            <div className="pListItem">
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square200/383595193.webp?k=a877f1d8428e206964d449ba38944eff2a3b371c435dc93c85df71a8f8c36992&o=&s=1" alt="" className="pListImg" />
+                <div className="pListTitles">
+                    <h1>Apartments</h1>
+                    <h2>234 hotels</h2>
+                </div>
+            </div>
+            <div className="pListItem">
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square200/265509733.webp?k=d2d2d237c76c32772707123a0a5e32c30953d34b64f57f02f02bf8881ecd9aef&o=&s=1" alt="" className="pListImg" />
+                <div className="pListTitles">
+                    <h1>Villas</h1>
+                    <h2>234 hotels</h2>
+                </div>
+            </div>
+            <div className="pListItem">
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square200/383654453.webp?k=2b7a68296c5e3babccd6c16d8bccf6709bcd1ada5cb0d7a7a82cfd9100596c5d&o=&s=1" alt="" className="pListImg" />
+                <div className="pListTitles">
+                    <h1>Cabins</h1>
+                    <h2>234 hotels</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default PropertyList
