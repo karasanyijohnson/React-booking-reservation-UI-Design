ed4b8124df1b5a0fa6ecb88fa178a536c66ff6ae
Johnson <johnsonkarasanyi@gmail.com>
2022-08-23 10:59:51 +0200

    style featured Properties component

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -2,19 +2,8 @@ import './featuredProperties.css'
 const FeaturedProperties =()=>{
     return (
         <div className='fp'>
-            <img src="https://cf.bstatic.com/xdata/images/hotel/square200/126772087.webp?k=f9fe95a40b34d0ca2fc0f9dfc46501b97b35dcf864a4c045a932c6a30a22439a&o=&s=1" 
-            alt=""
-             className="fpImg" 
-             />
-            <span className="fpName">Aparthotel Stare Miasto</span>
-            <span className="fpCity">Madrid</span>
-            <span className="fpPrice">Starting from 120$</span>
-            <div className="fpRating">
-                <button>8.9</button>
-                <span>Excellent</span>
-            </div>
-            <div className="fpItem">
-            <img src="https://cf.bstatic.com/xdata/images/hotel/square200/126772087.webp?k=f9fe95a40b34d0ca2fc0f9dfc46501b97b35dcf864a4c045a932c6a30a22439a&o=&s=1" 
+           <div className="fpItem">
+            <img src="https://cf.bstatic.com/xdata/images/hotel/square200/303890276.webp?k=08b96972cdde47371db139375a4291108d9241cc6beb26c85816b7e63cde4a02&o=&s=1" 
             alt=""
              className="fpImg" 
              />
@@ -27,7 +16,7 @@ const FeaturedProperties =()=>{
             </div>
             </div>
             <div className="fpItem">
-            <img src="https://cf.bstatic.com/xdata/images/hotel/square200/126772087.webp?k=f9fe95a40b34d0ca2fc0f9dfc46501b97b35dcf864a4c045a932c6a30a22439a&o=&s=1" 
+            <img src="https://cf.bstatic.com/xdata/images/hotel/square200/303890276.webp?k=08b96972cdde47371db139375a4291108d9241cc6beb26c85816b7e63cde4a02&o=&s=1" 
             alt=""
              className="fpImg" 
              />
@@ -40,7 +29,33 @@ const FeaturedProperties =()=>{
             </div>
             </div>
             <div className="fpItem">
-            <img src="https://cf.bstatic.com/xdata/images/hotel/square200/126772087.webp?k=f9fe95a40b34d0ca2fc0f9dfc46501b97b35dcf864a4c045a932c6a30a22439a&o=&s=1" 
+            <img src="https://cf.bstatic.com/xdata/images/hotel/square200/303890276.webp?k=08b96972cdde47371db139375a4291108d9241cc6beb26c85816b7e63cde4a02&o=&s=1" 
+            alt=""
+             className="fpImg" 
+             />
+            <span className="fpName">Aparthotel Stare Miasto</span>
+            <span className="fpCity">Madrid</span>
+            <span className="fpPrice">Starting from 120$</span>
+            <div className="fpRating">
+                <button>8.9</button>
+                <span>Excellent</span>
+            </div>
+            </div>
+            <div className="fpItem">
+            <img src="https://cf.bstatic.com/xdata/images/hotel/square200/303890276.webp?k=08b96972cdde47371db139375a4291108d9241cc6beb26c85816b7e63cde4a02&o=&s=1" 
+            alt=""
+             className="fpImg" 
+             />
+            <span className="fpName">Aparthotel Stare Miasto</span>
+            <span className="fpCity">Madrid</span>
+            <span className="fpPrice">Starting from 120$</span>
+            <div className="fpRating">
+                <button>8.9</button>
+                <span>Excellent</span>
+            </div>
+            </div>
+            <div className="fpItem">
+            <img src="https://cf.bstatic.com/xdata/images/hotel/square200/303890276.webp?k=08b96972cdde47371db139375a4291108d9241cc6beb26c85816b7e63cde4a02&o=&s=1" 
             alt=""
              className="fpImg" 
              />
